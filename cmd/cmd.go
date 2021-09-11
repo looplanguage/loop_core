9dd03623ea11bf3921d1111ad424dbb6ab14660f
Kanersps <kanepetra@gmail.com>
2021-09-11 14:29:43 +0200

    tweak: support executing files & write first example

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,10 +7,40 @@ import (
 	"git.kanersps.pw/loop/lexer"
 	"git.kanersps.pw/loop/models/object"
 	"git.kanersps.pw/loop/parser"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
 func Execute() {
+	// Check if file is specified, run if it is
+	if len(os.Args) == 2 {
+		input, err := ioutil.ReadFile(os.Args[1])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		env := object.CreateEnvironment()
+
+		l := lexer.Create(string(input))
+		p := parser.Create(l)
+
+		program := p.Parse()
+
+		if len(p.Errors) > 0 {
+			for _, err := range p.Errors {
+				fmt.Println(err)
+			}
+		} else {
+			evaluated := evaluator.Eval(program, env)
+
+			fmt.Println(evaluated.Inspect())
+		}
+
+		os.Exit(0)
+	}
+
 	// REPL here for testing while developing
 	reader := bufio.NewScanner(os.Stdin)
 
